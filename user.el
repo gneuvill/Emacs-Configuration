d5ab86529cad7c87451f4fddb5830e8a5973eb40
Grégoire Neuville <gregoire.neuville@gmail.com>
2012-03-26 23:33:59 +0200

    elisp-slime-nav + gitconfig for my-ensime-git

--- user.el
+++ user.el
@@ -88,6 +88,10 @@
 ;;(setq org-default-notes-file (concat org-directory "ori-oai-ao.org"))
 (define-key global-map "\C-cr" 'org-remember)
 
+;; Slime-style navigation for Emacs Lisp (https://github.com/purcell/elisp-slime-nav)
+(require 'elisp-slime-nav)
+(add-hook 'emacs-lisp-mode-hook (lambda () (elisp-slime-nav-mode t)))
+
 ;; our functions
 (require 'my-functions)
 
