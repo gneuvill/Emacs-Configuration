618110208b4506d4494e6b7db03f45f535e62324
Grégoire Neuville <gregoire.neuville@gmail.com>
2013-09-10 17:47:21 +0200

    Minor

--- user.el
+++ user.el
@@ -78,7 +78,7 @@
 (add-to-list 'tramp-default-proxies-alist '("myenor" nil "/ssh:gneuvill@enor:"))
 (add-to-list 'tramp-default-proxies-alist '("lionel" nil "/ssh:llevague@129.20.129.69:"))
 
-;; a supplementary package archive
+;; supplementary packages archives
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                            ("marmalade" . "http://marmalade-repo.org/packages/")
                            ("melpa" . "http://melpa.milkbox.net/packages/")))
